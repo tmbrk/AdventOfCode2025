58fbc43d1f4ea12f5b81f69afec417828b6f3647
TB <tombroek@gmail.com>
2026-01-21 15:28:38 +0100

    Refine Day 5 boilerplate to parse input correctly

--- day5/solution.py
+++ day5/solution.py
@@ -5,18 +5,23 @@ from numpy.lib import _stride_tricks_impl
 
 
 def parse_input(filename):
-    fresh = []
-    prods = []
-    with open(filename, "r") as f:
-        for line in f:
-            if "-" in line:
-                fresh.append(list(map(int, line.strip().split("-"))))
-            elif "\n" == line:
-                continue
-            else:
-                prods.append(int(line.strip()))
+    with open(filename, 'r') as f:
+        content = f.read()
+    
+    parts = content.split('\n\n')
+    
+    ranges_str = parts[0].strip().split('\n')
+    numbers_str = parts[1].strip().split('\n') if len(parts) > 1 else []
 
-    return (fresh, prods)
+    ranges = []
+    for r_str in ranges_str:
+        if r_str:
+            start_str, end_str = r_str.split('-')
+            ranges.append((int(start_str), int(end_str)))
+            
+    numbers = [int(n) for n in numbers_str if n]
+            
+    return ranges, numbers
 
 
 def part1(data):
@@ -29,19 +34,37 @@ def part1(data):
     return len(result)
 
 
+def reduce_fresh(fresh, result=[]):
+    if not fresh:
+        return result
+    elif not result:
+        return reduce_fresh(fresh, [fresh[0]])
+    else:
+        check = fresh.pop(0)
+        if result[-1][1] >= check[0] and result[-1][1] <= check[1]:
+            result[-1][1] = check[1]
+        elif result[-1][1] < check[0]:
+            result.append(check)
+        return reduce_fresh(fresh, result)
+
+
 def part2(data):
-    # TODO: Implement Part 2
-    return None
+    data[0].sort(key=lambda x: x[0])
+    result = reduce_fresh(data[0])
+    out = 0
+    for x in result:
+        out += x[1] + 1 - x[0]
+
+    return out
 
 
 def solve(filename):
-    data = parse_input(filename)
-    print(data)
+    ranges, numbers = parse_input(filename)
 
-    p1 = part1(data)
+    p1 = part1(ranges)
     print(f"Part 1: {p1}")
 
-    p2 = part2(data)
+    p2 = part2(numbers)
     print(f"Part 2: {p2}")
 
 

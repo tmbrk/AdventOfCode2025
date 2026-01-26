3bd5cdd92cc69cc6942b4bfcdf0f118f772aa0f6
TB <tombroek@gmail.com>
2026-01-26 13:33:04 +0100

    Remove Rust build artifacts and add .gitignore

--- day8/rust/target/debug/deps/day8-0939ddb668a61f1b.d
+++
@@ -1,9 +0,0 @@
-/var/home/tom/Projects/AdventOfCode2025/day8/rust/target/debug/deps/day8-0939ddb668a61f1b.d: src/main.rs Cargo.toml
-
-/var/home/tom/Projects/AdventOfCode2025/day8/rust/target/debug/deps/libday8-0939ddb668a61f1b.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
